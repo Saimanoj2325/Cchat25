9bc5a38d069206c27e713314d7ba3a8ae7136880
Saimanoj2325 <025saimanojkura@gmail.com>
2025-06-26 17:28:19 +0530

    check2

--- Frontend/src/components/ChatInterface.tsx
+++ Frontend/src/components/ChatInterface.tsx
@@ -18,7 +18,7 @@ export const ChatInterface: React.FC<ChatInterfaceProps> = ({ user, onLeave }) =
   const [typingUsers, setTypingUsers] = useState<string[]>([]);
 
   useEffect(() => {
-    const newSocket = io('http://localhost:3000');
+    const newSocket = io('https://cchat25.onrender.com');
     setSocket(newSocket);
 
     newSocket.on('connect', () => {
